7c49ff7a1653ee14f0dbde1c124e032010cbb630
stugl <stu@giglabs.io>
2023-08-26 14:33:13 -0700

    adjusted response.js

--- pages/api/response.js
+++ pages/api/response.js
@@ -1,14 +1,17 @@
 // This is a simple in-memory store for our messages.
 // In a production scenario, consider using a database.
-let messages = [];
+let storedMessages = [];
 
 export default function handler(req, res) {
   if (req.method === 'POST') {
     const { message } = req.body;
-    messages.push({ user: 'Bot', message });
+    storedMessages.push({ user: 'Bot', message });
     res.status(200).json({ status: 'success' });
   } else {
     res.status(405).json({ error: 'Method not allowed' });
   }
 }
-  
+
+export const messages = [
+    // ... your messages here
+];
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const API_ENDPOINT = 'https://api.magickml.com/api';
 const AGENT_ID = '0fdcf186-0a5a-4919-9bef-1bc543909b81';
 const API_KEY = '6d41694cc0e084bca0ce5dda74b670da';
 const POLL_INTERVAL = 5000;
+
+
 
 export default function Home() {
   const [message, setMessage] = useState('');
