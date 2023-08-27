02b581ea148f5a3c98a934db5cf88b25c6bd9aab
stugl <stu@giglabs.io>
2023-08-27 15:22:10 -0700

    style update 2

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,11 @@ const API_ENDPOINT = 'https://api.magickml.com/api';
 const AGENT_ID = '9acc0d41-e867-4ae2-8ead-2f60cf003ae4';
 const API_KEY = '3ad7bec48d6b8df5b1b3b1085cba7ec2';
 const POLL_INTERVAL = 5000;
+const chatTitle = "Matrix Chat"; // Editable chat title
 
 export default function Home() {
     const [message, setMessage] = useState('');
     const [chat, setChat] = useState([]);
-    const [chatTitle, setChatTitle] = useState('Your Chat Title Here');
 
   const sendMessage = async () => {
     // Add the user's message to the chat immediately
@@ -71,19 +71,16 @@ export default function Home() {
   
 
   return (
-    <div style={{ height: '100vh', background: 'linear-gradient(to right, #ff6a00, #ee0979)', padding: '50px', fontFamily: 'Comic Sans MS' }}>
-      <input 
-        value={chatTitle} 
-        onChange={(e) => setChatTitle(e.target.value)} 
-        placeholder="Set your chat title..."
-        style={{ marginBottom: '10px', padding: '10px', borderRadius: '5px', border: '2px solid white' }}
-      />
-      <h1 style={{ color: 'white', textAlign: 'center' }}>{chatTitle}</h1>
-      <img src="https://framerusercontent.com/images/OONMMucdnpD4xGWMoRzpdMibGJ4.png?scale-down-to=512" alt="Y2K style image" style={{ width: '100%', marginBottom: '20px' }} />
-      <div style={{ backgroundColor: 'white', padding: '20px', borderRadius: '10px', boxShadow: '0px 0px 20px rgba(0, 0, 0, 0.2)' }}>
+    <div style={{ height: '100vh', backgroundColor: 'black', padding: '50px', fontFamily: 'Courier New, Courier, monospace' }}>
+      <h1 style={{ color: '#00FF00' }}>{chatTitle}</h1>
+      <img src="https://framerusercontent.com/images/OONMMucdnpD4xGWMoRzpdMibGJ4.png?scale-down-to=512" alt="Matrix Icon" />
+      <div style={{ color: '#00FF00', fontSize: '12px', marginBottom: '20px' }}>
+        Welcome to Matrix Chat - A sleek y2k style chat interface.
+      </div>
+      <div style={{ backgroundColor: '#1A1A1A', padding: '20px', borderRadius: '10px' }}>
         {chat.map((msg, index) => (
           <div key={index}>
-            <strong>{msg.user}:</strong> {msg.message}
+            <strong style={{ color: msg.user === 'You' ? '#00FF00' : '#FF4500' }}>{msg.user}:</strong> {msg.message}
           </div>
         ))}
         <input
@@ -91,18 +88,10 @@ export default function Home() {
           onChange={(e) => setMessage(e.target.value)}
           onKeyPress={handleKeyPress}
           placeholder="Type your message..."
-          style={{ width: '80%', padding: '10px', margin: '10px 0', borderRadius: '5px', border: '2px solid #ff6a00' }}
+          style={{ width: '80%', padding: '10px', margin: '10px 0', backgroundColor: '#333', color: '#00FF00', border: '1px solid #00FF00' }}
         />
-        <button 
-          onClick={sendMessage} 
-          style={{ background: 'linear-gradient(to right, #ff6a00, #ee0979)', color: 'white', padding: '10px 20px', borderRadius: '5px', border: 'none', cursor: 'pointer', boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.2)' }}
-        >
-          Send
-        </button>
+        <button style={{ backgroundColor: '#00FF00', color: 'black', border: 'none', padding: '5px 10px', borderRadius: '5px' }} onClick={sendMessage}>Send</button>
       </div>
-      <p style={{ marginTop: '20px', color: 'white', fontSize: '14px' }}>
-        Welcome to Y2K Chat! This app lets you chat with our advanced AI bot. Just type your message and hit send.
-      </p>
     </div>
   );
 
