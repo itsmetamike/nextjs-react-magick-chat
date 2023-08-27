aa87eef3470229a4581969e43620529f4e4ea426
stugl <stu@giglabs.io>
2023-08-27 15:37:30 -0700

    style update 4 - framer motion

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,50 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const API_ENDPOINT = 'https://api.magickml.com/api';
 const AGENT_ID = '9acc0d41-e867-4ae2-8ead-2f60cf003ae4';
 const API_KEY = '3ad7bec48d6b8df5b1b3b1085cba7ec2';
 const POLL_INTERVAL = 5000;
 const chatTitle = "Matrix Chat"; // Editable chat title
+const Container = styled(motion.div)`
+  height: 100vh;
+  padding: 50px;
+  background: black;
+  font-family: 'Courier New', monospace;
+  color: limegreen;
+`;
+
+const Message = styled(motion.div)`
+  &:hover {
+    opacity: 0.7;
+  }
+`;
+
+const Input = styled.input`
+  width: 80%;
+  padding: 10px;
+  margin: 10px 0;
+  background-color: black;
+  color: limegreen;
+  border: 1px solid limegreen;
+  &:focus {
+    outline: none;
+    border: 1px solid white;
+  }
+`;
+
+const Button = styled.button`
+  background-color: limegreen;
+  border: none;
+  padding: 5px 10px;
+  cursor: pointer;
+  transition: opacity 0.3s;
+  &:hover {
+    opacity: 0.8;
+  }
+`;
 
 export default function Home() {
     const [message, setMessage] = useState('');
@@ -71,26 +110,22 @@ export default function Home() {
   
 
   return (
-    <div style={{ height: '100vh', background: 'black', padding: '50px', fontFamily: 'Courier New, monospace', color: 'limegreen' }}>
-      <h1>{chatTitle}</h1>
-      <img src="https://framerusercontent.com/images/OONMMucdnpD4xGWMoRzpdMibGJ4.png?scale-down-to=512" alt="Y2K Image" style={{ margin: '20px 0', maxWidth: '100%' }} />
-      <p>Welcome to the Y2K ChatBot - the ultimate matrix-inspired chat interface. Engage in a conversation with our AI and dive deep into the digital world!</p>
-      <div style={{ backgroundColor: 'black', padding: '20px', border: '2px solid limegreen', borderRadius: '10px' }}>
-        {chat.map((msg, index) => (
-          <div key={index}>
-            <strong>{msg.user}:</strong> {msg.message}
-          </div>
-        ))}
-        <input
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          onKeyPress={handleKeyPress}
-          placeholder="Type your message..."
-          style={{ width: '80%', padding: '10px', margin: '10px 0', backgroundColor: 'black', color: 'limegreen', border: '1px solid limegreen' }}
-        />
-        <button onClick={sendMessage} style={{ backgroundColor: 'limegreen', border: 'none', padding: '5px 10px', cursor: 'pointer' }}>Send</button>
+    <Container initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+      <motion.h1 initial={{ y: -50 }} animate={{ y: 0 }} transition={{ duration: 0.5 }}>{chatTitle}</motion.h1>
+      <motion.img src="https://framerusercontent.com/images/OONMMucdnpD4xGWMoRzpdMibGJ4.png?scale-down-to=512" alt="Image" initial={{ scale: 0 }} animate={{ scale: 1 }} transition={{ duration: 0.5 }} />
+      <motion.p initial={{ y: 50 }} animate={{ y: 0 }} transition={{ duration: 0.5 }}>Welcome to the chat interface.</motion.p>
+      <div style={{ padding: '20px', border: '2px solid limegreen', borderRadius: '10px' }}>
+        <AnimatePresence>
+          {chat.map((msg, index) => (
+            <Message key={index} initial={{ opacity: 0, y: 50 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: -50 }}>
+              <strong>{msg.user}:</strong> {msg.message}
+            </Message>
+          ))}
+        </AnimatePresence>
+        <Input value={message} onChange={(e) => setMessage(e.target.value)} onKeyPress={handleKeyPress} placeholder="Type your message..." />
+        <Button onClick={sendMessage}>Send</Button>
       </div>
-    </div>
+    </Container>
   );
 
 
