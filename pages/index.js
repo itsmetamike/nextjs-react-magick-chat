18b08241c2deb8a67a2900ff6c16acddbaafea61
stugl <stu@giglabs.io>
2023-08-27 14:39:19 -0700

    revert to old build

--- pages/index.js
+++ pages/index.js
@@ -83,16 +83,3 @@ export default function Home() {
     </div>
   );
 }
-
-getMessages.js
-
-// Use the same in-memory store from before
-import { messages } from './response';
-
-export default function handler(req, res) {
-  if (req.method === 'GET') {
-    res.status(200).json(messages);
-  } else {
-    res.status(405).json({ error: 'Method not allowed' });
-  }
-}
